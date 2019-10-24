442eff66594f58e13bc69ccca515117a8c9c9d24
Rahul <rahulreghunath11@gmail.com>
2019-10-24 23:53:14 +0530

    Initial setup

---
+++ src/VueDoubleList.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <select name="" id="">
+    <option value="">Hello World</option>
+  </select>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'VueDoubleList',
+  };
+</script>
+
+<style scoped>
+
+</style>
